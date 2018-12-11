f50f9887f1eb39c269a502a00e598aca9307292a
liuchengyuan123 <34617968+liuchengyuan123@users.noreply.github.com>
2018-12-11 21:49:02 +0800

    Update 后缀自动机.cpp

--- String/后缀自动机.cpp
+++ String/后缀自动机.cpp
@@ -47,27 +47,6 @@ void insert(int x)
                 ch[p][x] = nq, p = fa[p];
         }
     }
-    /*
-    int p=last;
-    int np = last = ++tot;
-    len[np] = len[p] + 1;
-    for(;p&&!ch[p][x];p=fa[p])    ch[p][x]=np;
-    if(!p)    fa[np]=root;
-    else
-    {
-        int q=ch[p][x];
-        if(len[q]==len[p]+1)    
-            fa[np]=q;
-        else
-        {
-            int nq=++tot;
-            fa[nq]=fa[q];fa[q]=fa[np]=nq;
-            memcpy(ch[nq],ch[q],sizeof(ch[nq]));len[nq]=len[p]+1;
-            for(;p&&ch[p][x]==q;p=fa[p])    ch[p][x]=nq;
-        }
-    }
-    ed[np]=1;
-    */
 }
 
 char s[N];
@@ -103,25 +82,6 @@ int main()
             q.push(fa[lcy]);
         }
     }
-    /*
-    int i,t;
-    for(i=1;i<=mem;i++)    ++in[par[i]];
-    for(i=1;i<=mem;i++)
-        if(!in[i])
-            q.push(i);
-    while(!q.empty())
-    {
-        t=q.front();q.pop();
-        if(sz[t]>1)    ans=max(ans,LL(sz[t])*len[t]);
-        if(par[t])
-        {
-            sz[par[t]]+=sz[t];
-            --in[par[t]];
-            if(!in[par[t]])    q.push(par[t]);
-        }
-    }
-    */
-    printf("%lld", ans);
-    cin >> ans;
+    printf("%lld\n", ans);
     return 0;
 }
